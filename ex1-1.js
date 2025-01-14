5289bb7256459678aa23fa94be2390957837aee6
sakuratanaka <167832973+sakuratanaka@users.noreply.github.com>
2025-01-14 21:09:16 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,5 +1,5 @@
 function imgchange1() {
-    document.getElementById('monstar').src = './image/2.jpeg';
+    document.getElementById('monstar').src = './image/2.jpg';
 }
 function imgchange2() {
     document.getElementById('monstar').src = './image/1.jpg';
